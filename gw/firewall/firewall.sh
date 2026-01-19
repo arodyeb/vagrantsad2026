29f680906e69ec4ce4187a46a096ae82bc942201
arodyeb <arodyeb611@g.educaand.es>
2026-01-19 10:55:04 +0100

    Añadidas reglas al cortafuegos

--- gw/firewall/firewall.sh
+++ gw/firewall/firewall.sh
@@ -22,7 +22,23 @@ iptables -P OUTPUT DROP
 ##############################
 # Reglas de protección local
 ##############################
-# 1. Permitir tráfico de loopback
+# L1. Permitir tráfico de loopback
+iptables -A OUTPUT -o lo -j ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+
+# L2. Permitir ping a cualquier máquina interna o externa
+iptables -A OUTPUT -p icmp --icmp-type echo-request -j ACCEPT
+iptables -A INPUT -p icmp --icmp-type echo-reply -j ACCEPT
+
+# L3. Permitir que me hagan ping desde LAN y DMZ
+iptables -A INPUT -i eth2 -s 172.1.8.0/24 -p icmp --icmp-type echo-request -j ACCEPT
+iptables -A INPUT -i eth3 -s 172.2.8.0/24 -p icmp --icmp-type echo-request -j ACCEPT
+iptables -A OUTPUT -o eth2 -s 172.1.8.1 -p icmp --icmp-type echo-reply -j ACCEPT
+iptables -A OUTPUT -o eth3 -s 172.2.8.1 -p icmp --icmp-type echo-reply -j ACCEPT
+
+# L4. Permitir consultas DNS
+iptables -A OUTPUT -o eth0 -p udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A INPUT -i eth0 -p udp --sport 53 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
 ##############################
 # Reglas de protección red
